f1e7b10b5938d08a6dd7c36f5f8c2d53fd9cee5f
Confucius <alyssa.pidge@live.com.au>
2023-08-13 10:31:04 +0930

    writing code for schema

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+
+inquirer.createPromptModule([
+    {
+        type: 
+        message: 'What would you like to do?'
+    }, 
+    {
+        type: 
+        message: 'What is the name of the department?'
+    },
+    {
+        type: 
+        message: 'What is the name of the role?'
+    },
+    {
+        type: 
+        message: 'What is the salary of the role?'
+    },
+    {
+        type: 
+        message: 'Which department does the role belong to?'
+    },
+])
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//connect to DB:
+//connect to DB using mysql2:
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -18,6 +18,18 @@ const db = mysql.createConnection(
   },
   console.log("Connected to the employee_db database.")
 );
+
+//query -
+
+connection.execute(
+  "SELECT * FROM 'department' WHERE 'name' = ? AND 'id' = ?",
+  function (err, results, fields) {
+    console.log(results);
+    console.log(fields);
+  }
+);
+
+con.promise().query("Select 1");
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
